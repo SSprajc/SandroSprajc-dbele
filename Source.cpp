b9a7699327411f8f555972fa2e8804640230e8eb
SSprajc <82279963+SSprajc@users.noreply.github.com>
2021-04-11 22:03:05 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(VideoMode(800, 600), "Hello, SFML world!");
+	window.setFramerateLimit(60);
+	Cvijet cvijet(&window);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		cvijet.draw(window);
+		window.display();
+	}
+
+	return 0;
+}
